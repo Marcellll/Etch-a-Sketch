21dcad5109161b3a29ae8804d4548613e183a2ce
Marcel Home WSL <beyer.abel.marcel@outlook.de>
2025-11-22 18:13:30 +0100

    Added a dynamic view of the grid size slider

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,6 +1,6 @@
 const mainContainer = document.querySelector(".container")
 const squareBorderSize = 1; 
-let size = prompt("Enter size of the boxes", 12)
+let size = prompt("Enter size of the boxes", 16)
 
 for (let index = 0; index < size * size; index++) {
     let square = document.createElement("div");
@@ -10,4 +10,18 @@ for (let index = 0; index < size * size; index++) {
     square.style.border = `${squareBorderSize}px solid grey`
     square.style.background = "white"
     mainContainer.appendChild(square)
-}0
+}
+
+//Eventlistener to display the grid size to the user
+const sliderInput = document.querySelector(".slider")
+const sliderOutput = document.querySelectorAll(".gridSizeOutput")
+//Initialization
+sliderOutput.forEach(element => {
+    element.textContent = sliderInput.value
+});
+//When a change occurs change the value as well
+sliderInput.addEventListener("input", (event) => {
+    sliderOutput.forEach(element => {
+        element.textContent = event.target.value
+    });
+})
