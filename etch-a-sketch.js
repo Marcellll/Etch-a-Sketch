88e3a1e76f8a4c1927a5ca16dcca944b4713cd8f
Marcel Home WSL <beyer.abel.marcel@outlook.de>
2025-11-22 18:27:47 +0100

    The slider changes the grid size dynamically

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,27 +1,38 @@
 const mainContainer = document.querySelector(".container")
 const squareBorderSize = 1; 
-let size = prompt("Enter size of the boxes", 16)
 
-for (let index = 0; index < size * size; index++) {
-    let square = document.createElement("div");
-    square.className = "square"
-    square.style.width = `calc(100% / ${size} - ${squareBorderSize*2}px)`
-    square.style.height = `calc(100% / ${size} - ${squareBorderSize*2}px)`
-    square.style.border = `${squareBorderSize}px solid grey`
-    square.style.background = "white"
-    mainContainer.appendChild(square)
+function createGridElement(gridSize) {
+    //Cleaning up the grid for a new one
+    while(mainContainer.firstChild) {
+        mainContainer.removeChild(mainContainer.lastChild)
+    }
+
+    for (let index = 0; index < gridSize * gridSize; index++) {
+        let square = document.createElement("div");
+        square.className = "square"
+        square.style.width = `calc(100% / ${gridSize} - ${squareBorderSize*2}px)`
+        square.style.height = `calc(100% / ${gridSize} - ${squareBorderSize*2}px)`
+        square.style.border = `${squareBorderSize}px solid grey`
+        square.style.background = "white"
+        mainContainer.appendChild(square)
+    }
 }
 
 //Eventlistener to display the grid size to the user
 const sliderInput = document.querySelector(".slider")
 const sliderOutput = document.querySelectorAll(".gridSizeOutput")
-//Initialization
-sliderOutput.forEach(element => {
-    element.textContent = sliderInput.value
-});
 //When a change occurs change the value as well
 sliderInput.addEventListener("input", (event) => {
     sliderOutput.forEach(element => {
         element.textContent = event.target.value
     });
+    createGridElement(event.target.value)
 })
+
+
+
+//Initialization
+sliderOutput.forEach(element => {
+    element.textContent = sliderInput.value
+});
+createGridElement(16)
